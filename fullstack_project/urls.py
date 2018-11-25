e190ecf52fe9dcd871b3a30038a6eaed82f3d387
shraddha bhise <shraddhabhise838@gmail.com>
2018-11-24 16:33:04 -0800

    Added date posted, changed logo and look of UI

--- fullstack_project/urls.py
+++ fullstack_project/urls.py
@@ -25,9 +25,8 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LoginView.as_view(template_name='logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('', include('recipes.urls')),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
